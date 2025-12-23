eaaf0d979007d9eb0eebb26ac3792c32d1aa5451
MM2-ScriptsZ <stfuhis1@gmail.com>
2025-12-23 20:13:20 +0800

    Update linkvertise.js

--- api/linkvertise.js
+++ api/linkvertise.js
@@ -3,10 +3,13 @@ import { redis } from "./_redis.js";
 export default async function handler(req, res) {
   const { session } = req.query;
 
+  // If Linkvertise sends only hash → ignore
   if (!session) {
     return res.redirect("/");
   }
 
+  // SAVE PROGRESS
   await redis.set(`progress:${session}`, "1", { ex: 600 });
+
   return res.redirect(`/?session=${session}`);
 }
