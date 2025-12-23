da70a521ba6edb5861e13bdc60c30a106e7497b7
MM2-ScriptsZ <stfuhis1@gmail.com>
2025-12-23 18:54:29 +0800

    Update admin-list-keys.js

--- api/admin-list-keys.js
+++ api/admin-list-keys.js
@@ -1,6 +1,19 @@
-import { readDB } from "./_db.js";
+import { redis } from "./_redis.js";
 
-export default function handler(req, res) {
-  const db = readDB();
-  res.json({ keys: db.keys });
+export default async function handler(req, res) {
+  const session = req.headers["x-admin-session"];
+  if (!session) return res.status(401).end();
+
+  const ok = await redis.get(`admin:session:${session}`);
+  if (!ok) return res.status(401).end();
+
+  const keys = {};
+  const list = await redis.keys("key:*");
+
+  for (const k of list) {
+    const data = await redis.hgetall(k);
+    keys[k.replace("key:", "")] = data;
+  }
+
+  res.json({ keys });
 }
